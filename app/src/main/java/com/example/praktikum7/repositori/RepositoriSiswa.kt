2cdcf2fed0d6b595c6ea66a2530b2736a14f2df0
halimbagusalhakim <halsat132@gmail.com>
2025-11-27 13:16:27 +0700

    mendefinisikan interface RepositoriSiswa sebagai layer abstraksi untuk akses data siswa menggunakan Flow dan operasi suspend. OfflineRepositoriSiswa mengimplementasikan interface tersebut dengan memanfaatkan SiswaDao dari Room untuk mengambil dan menambahkan data siswa ke database lokal.

---
+++ app/src/main/java/com/example/praktikum7/repositori/RepositoriSiswa.kt
@@ -0,0 +1,21 @@
+package com.example.praktikum7.repositori
+
+import com.example.praktikum7.room.Siswa
+import kotlinx.coroutines.flow.Flow
+
+interface RepositoriSiswa {
+    fun getAllSiswaStream(): Flow<List<Siswa>>
+
+    suspend fun insertSiswa(siswa: Siswa)
+}
+
+class OfflineRepositoriSiswa(
+    private val siswaDao: SiswaDao
+): RepositoriSiswa{
+    override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao
+        .getAllSiswa()
+
+    override suspend fun insertSiswa(siswa: Siswa) = siswaDao
+        .insert(siswa)
+
+}
