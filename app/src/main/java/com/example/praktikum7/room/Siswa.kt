4980ff6ce60c863142338fcc85adecd11aac179d
halimbagusalhakim <halsat132@gmail.com>
2025-11-27 12:47:13 +0700

    membuat variabel id dengan inisaisi value 0 dan primarykey auto generate

--- app/src/main/java/com/example/praktikum7/room/Siswa.kt
+++ app/src/main/java/com/example/praktikum7/room/Siswa.kt
@@ -1,6 +1,10 @@
 package com.example.praktikum7.room
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "tblSiswa")
-data class Siswa()
+data class Siswa(
+    @PrimaryKey(autoGenerate = true)
+    val id: int = 0
+)
