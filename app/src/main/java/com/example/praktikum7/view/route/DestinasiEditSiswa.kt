b5d31204aa601c028ed11cd283cec5337fb1f387
halimbagusalhakim <halsat132@gmail.com>
2025-12-04 13:36:13 +0700

    membuat object destinasiEditSIswa

--- app/src/main/java/com/example/praktikum7/view/route/DestinasiEditSiswa.kt
+++ app/src/main/java/com/example/praktikum7/view/route/DestinasiEditSiswa.kt
@@ -1,6 +1,10 @@
 package com.example.praktikum7.view.route
 
-object DestinasiEditSiswa {
+import com.example.praktikum7.R
 
-
+object DestinasiEditSiswa : DestinasiNavigasi {
+    override val route = "item_edit"
+    override val titleRes = R.string.edit_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
 }
